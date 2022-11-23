2842d4dd854670e901def077843583582e66ca48
choogoo <104893934+choogoo@users.noreply.github.com>
2022-11-23 17:44:46 +0800

    模块化拆分

--- index.ts
+++ index.ts
@@ -4,23 +4,8 @@
 /* eslint-disable eqeqeq */
 /* eslint-disable camelcase */
 /* eslint-disable sort-keys */
-/**
- * Wechaty - Conversational RPA SDK for Chatbot Makers.
- *  - https://github.com/wechaty/wechaty
- */
-// https://stackoverflow.com/a/42817956/1123955
-// https://github.com/motdotla/dotenv/issues/89#issuecomment-587753552
 import 'dotenv/config.js'
-
-// import openai from 'openai-sdk'
-// const {
-//   init,
-//   chat,
-//   nlp,
-// } = openai
-
 import fs from 'fs'
-
 import {
   init,
   chat,
@@ -53,24 +38,13 @@ import rp from 'request-promise'
 // import WechatyVikaPlugin from 'wechaty-vika-link'
 import WechatyVikaPlugin from './src/index.js'
 
-import excel2order from './src/excel.js'
 import { FileBox } from 'file-box'
-import path from 'path'
-
-import os from 'os'
-
 import configs from './config.js'
 import type { Puppet } from 'wechaty-puppet'
-
 import * as io from 'socket.io-client'
-
 import { VikaBot } from './src/vika.js'
-
-import { JsonRpcError } from 'json-rpc-peer'
-
-const __dirname = path.resolve()
-const userInfo = os.userInfo()
-const rootPath = `${userInfo.homedir}\\Documents\\WeChat Files\\`
+import { configData } from './src/plugins/im.js'
+import { wxai } from './src/plugins/wxai.js'
 
 let bot: any
 let sysConfig: any
@@ -87,12 +61,10 @@ const vikaConfig = {
   spaceName: configs.VIKA_SPACENAME,
   token: configs.VIKA_TOKEN,
 }
-console.debug(vikaConfig)
-
+// console.debug(vikaConfig)
 const vika = new VikaBot(vikaConfig)
 
-async function main () {
-
+async function main() {
   void await vika.checkInit()
   // 获取系统配置信息
   const configsRes = await vika.getConfig()
@@ -100,13 +72,11 @@ async function main () {
   log.info(sysConfig)
 
   configs.roomWhiteList = configsRes[1]
-
   configs.welcomeList = [
     '25108313781@chatroom',
     '25187527247@chatroom',
     '20641535286@chatroom',
   ] // 进群欢迎语白名单
-
   configs.linkWhiteList = [
     'ledongmao',
     'xxxxxxx',
@@ -114,13 +84,12 @@ async function main () {
 
   sysConfig = { ...configs, ...sysConfig }
   log.info(sysConfig)
-
   console.debug(sysConfig)
 
-  init({
-    TOKEN: sysConfig.WX_TOKEN,
-    EncodingAESKey: sysConfig.EncodingAESKey,
-  })
+  // init({
+  //   TOKEN: sysConfig.WX_TOKEN,
+  //   EncodingAESKey: sysConfig.EncodingAESKey,
+  // })
 
   const wechatyConfig: any = {
     // 网页版微信
@@ -148,46 +117,6 @@ async function main () {
 
   bot = WechatyBuilder.build(wechatyConfig[sysConfig.puppetName])
 
-  // IM相关配置
-  const configData = {
-    socket: '',
-    chatInfoEn: {
-      chatState: 'agent', // chat状态；robot 机器人、agent 客服
-      inputContent: '', // 输入框内容
-      msgList: [], // 消息列表
-      state: 'on', // 连接状态;on ：在线；off：离线
-      lastMsgShowTime: new Date(), // 最后一个消息的显示时间
-    }, // 会话信息，包括聊天记录、状态
-    clientChatEn: {
-      clientChatId: 'ledongmao',
-      clientChatName: '客服机器人',
-      avatarUrl: 'static/image/im_client_avatar.png',
-    }, // 当前账号的信息
-    serverChatEn: {
-      serverChatId: 'xiaop',
-      serverChatName: '小P',
-      avatarUrl: 'static/image/im_robot_avatar.png',
-    }, // 服务端chat信息
-    robotEn: {
-      robotName: '小旺',
-      avatarUrl: 'static/image/im_robot_avatar.png',
-    }, // 机器人信息
-    faqList: [
-      { title: '今天周几', content: '今天周一' },
-      { title: '今天周几', content: '今天周二' },
-      { title: '今天周几', content: '今天周三' },
-      { title: '今天周几', content: '今天周四' },
-      { title: '今天周几', content: '今天周五' },
-    ],
-    faqSelected: '-1',
-    inputContent_setTimeout: null, // 输入文字时在输入结束才修改具体内容
-    selectionRange: null, // 输入框选中的区域
-    shortcutMsgList: [], // 聊天区域的快捷回复列表
-    logoutDialogVisible: false, // 结束会话显示
-    transferDialogVisible: false, // 转接人工dialog
-    rateDialogVisible: false, // 评价dialog
-    leaveDialogVisible: false, // 留言dialog
-  }
   let socket: io
   if (sysConfig.imOpen) {
     socket = io.connect('http://localhost:3001')
@@ -247,7 +176,7 @@ async function main () {
    * @param {String} msg.content 消息内容
    * @param {Function} successCallback 添加消息后的回调
    */
-  function addChatMsg (msg: { role: any; contentType: any; content?: string; createTime?: any }, successCallback: { (): void; (): any }) {
+  function addChatMsg(msg: { role: any; contentType: any; content?: string; createTime?: any }, successCallback: { (): void; (): any }) {
     // 1.设定默认值
     msg.role = msg.role == undefined ? 'sys' : msg.role
     msg.contentType = msg.contentType == undefined ? 'text' : msg.contentType
@@ -281,7 +210,7 @@ async function main () {
    * 发送消息
    * @param {Object} rs 回调对象
    */
-  function sendMsg (rs: any) {
+  function sendMsg(rs: any) {
     const msg = rs.msg
     msg.role = 'client'
     msg.avatarUrl = configData.clientChatEn.avatarUrl
@@ -301,7 +230,7 @@ async function main () {
     //   addChatMsg(msg)
   }
 
-  async function onScan (qrcode: string, status: ScanStatus) {
+  async function onScan(qrcode: string, status: ScanStatus) {
     console.debug(qrcode)
     if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
       const qrcodeUrl = encodeURIComponent(qrcode)
@@ -315,7 +244,7 @@ async function main () {
       qrcodeTerminal.generate(qrcode, { small: true })  // show qrcode on console
 
       let uploadedAttachments = ''
-      let file:FileBox
+      let file: FileBox
       let filePath = 'qrcode.png'
       try {
         file = FileBox.fromQRCode(qrcode)
@@ -349,16 +278,19 @@ async function main () {
     }
   }
 
-  function onLogin (user: Contact) {
+  async function onLogin(user: Contact) {
     log.info('StarterBot', '%s login', user.payload)
     // log.info(JSON.stringify(user.payload))
+    const room = await bot.Room.findAll()
+    console.debug(room)
+    return room
   }
 
-  function onLogout (user: Contact) {
+  function onLogout(user: Contact) {
     log.info('StarterBot', '%s logout', user)
   }
 
-  async function onMessage (message: Message) {
+  async function onMessage(message: Message) {
 
     log.info('onMessage', JSON.stringify(message))
 
@@ -382,7 +314,7 @@ async function main () {
           const isInRoomWhiteList = sysConfig.roomWhiteList.includes(roomId)
           if (isInRoomWhiteList) {
             log.info('当前群在白名单内，请求问答...')
-            await wxai(room, message)
+            await wxai(sysConfig, bot,talker, room, message)
           } else {
             log.info('当前群不在白名单内，流程结束')
           }
@@ -390,7 +322,7 @@ async function main () {
 
         if (!sysConfig.roomWhiteListOpen) {
           log.info('系统未开启白名单，请求问答...')
-          await wxai(room, message)
+          await wxai(sysConfig, bot,talker, room, message)
         }
 
         // IM服务开启时执行
@@ -417,7 +349,7 @@ async function main () {
       }
 
       if ((!room || !room.id) && !isSelfMsg) {
-        await wxai(undefined, message)
+        await wxai(sysConfig, bot,talker, undefined, message)
       }
 
     } catch (e) {
@@ -476,208 +408,12 @@ async function main () {
   }
 }
 
-function roomlinker (message: { text: () => any }) {
+function roomlinker(message: { text: () => any }) {
   const text = message.text()
   return ''
 }
 
-async function wxai (room: Room | undefined, message: Message) {
-  const talker = message.talker()
-  //   const roomid = room ? room.id : ''
-  let text = message.text()
-  let answer: any = {}
-  if (message.type() === bot.Message.Type.Text && room) {
-    answer = await aibot(talker, room, text)
-  }
-
-  if (message.type() === bot.Message.Type.Text && !room) {
-    answer = await aibot(talker, undefined, text)
-  }
-
-  if (room && message.type() === bot.Message.Type.MiniProgram && !sysConfig.linkWhiteList.includes(talker.id)) {
-    const miniProgram = await message.toMiniProgram()
-    text = `${miniProgram.title()?.slice(0, 5)}是由群主或管理员所发布的小程序卡片消息吗？`
-    answer = await aibot(talker, room, text)
-  }
-
-  if (room && message.type() === bot.Message.Type.Url && !sysConfig.linkWhiteList.includes(talker.id)) {
-    const urllink = await message.toUrlLink()
-    text = `${urllink.title().slice(0, 5)}是由群主或管理员所发布的小程序卡片消息吗？`
-    answer = await aibot(talker, room, text)
-  }
-
-  log.info(JSON.stringify(answer))
-
-  if (answer.messageType) {
-    switch (answer.messageType) {
-      case types.Message.Text: {
-        log.info(`向 ${talker.name()} 发送消息...`)
-
-        if (room) {
-          answer = text.length > 20 ? (answer.text + '\n------------------------------\n' + talker.name() + ':' + text.slice(0, 10) + '...') : (answer.text + '\n------------------------------\n' + talker.name() + ':' + text)
-          await room.say(answer, ...[talker])
-        } else {
-          answer = answer.text
-          await message.say(answer)
-        }
-
-        break
-      }
-      case types.Message.Image: {
-        const fileBox = FileBox.fromUrl(answer.text.url)
-
-        if (room) {
-          await room.say(fileBox)
-        } else {
-          await message.say(fileBox)
-        }
-
-        break
-      }
-      case types.Message.MiniProgram: {
-
-        const miniProgram = new bot.MiniProgram({
-          appid: answer.text.appid,
-          title: answer.text.title,
-          pagePath: answer.text.pagepath,
-          // thumbUrl: answer.text.thumb_url,
-          thumbUrl: 'https://openai-75050.gzc.vod.tencent-cloud.com/openaiassets_afffe2516dac42406e06eddc19303a8d.jpg',
-          thumbKey: '42f8609e62817ae45cf7d8fefb532e83',
-        })
-
-        let sayRes: any
-        if (room) {
-          sayRes = await room.say(miniProgram)
-        } else {
-          sayRes = await message.say(miniProgram)
-        }
-
-        console.debug(sayRes)
-
-        break
-      }
-      default: {
-        break
-      }
-
-    }
-
-  }
-
-  if (message.type() === bot.Message.Type.Attachment) {
-    try {
-      const file = await message.toFileBox()
-      const fileName = file.name
-      // text = `${urllink.title().slice(0, 5)}是由群主或管理员所发布的小程序卡片消息吗？`
-      // answer = await aibot(talker, room, text)
-      if (fileName.split('.')[1] === 'xlsx') {
-        // log.info('file=============', file)
-        const filePath = __dirname + `\\cache\\${new Date().getTime() + fileName}`
-        // let filePath = `C:\\Users\\wechaty\\Documents\\WeChat Files\\wxid_0o1t51l3f57221\\FileStorage\\File\\2022-05\\${file.name}`
-        await file.toFile(filePath)
-        await wait(1000)
-        log.info('fileName=====', filePath)
-
-        await excel2order(filePath, message)
-      }
-    } catch (err) {
-      log.error('转换失败', err)
-    }
-
-  }
-
-  // if (message.type() === bot.Message.Type.Image) {
-  //   await wait(1000)
-  //   try {
-  //     const file = await message.toFileBox()
-  //     log.info('image=====', file)
-  //   } catch (err) {
-  //     log.error('image=====', err)
-  //   }
-
-  // }
-
-};
-
-async function aibot (talker: Contact, room: Room | undefined, query: string) {
-  // log.info('开始请求微信对话平台...')
-
-  const roomid = room?.id
-  const wxid = talker.id
-  const nickName = talker.name()
-  const topic = await room?.topic()
-  // log.info(opt)
-
-  let answer: any = {}
-  let answerJson: any
-
-  try {
-    const username = room ? (nickName + '/' + topic) : nickName
-    const userid = room ? (wxid + '/' + roomid) : wxid
-    const signature = genToken({
-      username,
-      userid,
-    })
-
-    let queryData: QueryData
-    if (sysConfig.DIFF_REPLY_ONOFF && room) {
-      queryData = {
-        signature,
-        query,
-        first_priority_skills: [topic || ''],
-        second_priority_skills: ['通用问题'],
-      }
-    } else {
-      queryData = {
-        signature,
-        query,
-        first_priority_skills: ['通用问题'],
-      }
-    }
-
-    const resMsg = await chatAibot(queryData)
-    log.info(JSON.stringify(resMsg))
-
-    if (resMsg.msgtype && resMsg.confidence > 0.8) {
-      switch (resMsg.msgtype) {
-        case 'text':
-          answer = {
-            messageType: types.Message.Text,
-            text: resMsg.answer,
-          }
-          break
-        case 'miniprogrampage':
-          answerJson = JSON.parse(resMsg.answer)
-          answer = {
-            messageType: types.Message.MiniProgram,
-            text: answerJson.miniprogrampage,
-          }
-          break
-        case 'image':
-          answerJson = JSON.parse(resMsg.answer)
-          answer = {
-            messageType: types.Message.Image,
-            text: answerJson.image,
-          }
-          break
-        default:
-          log.info(JSON.stringify({ msg: '没有命中关键字', nickName, topic, roomid, query }))
-          break
-      }
-
-      if (sysConfig.DIFF_REPLY_ONOFF && room && (resMsg.skill_name !== topic && resMsg.skill_name !== '通用问题')) {
-        answer = {}
-      }
-      return answer
-    }
-    return answer
-  } catch (err) {
-    log.error(JSON.stringify(err))
-    return answer
-  }
-}
-
-async function doLog (params: any) {
+async function doLog(params: any) {
   const uri = 'http://127.0.0.1:1880/log'
   const method = 'POST'
   const opt = {
