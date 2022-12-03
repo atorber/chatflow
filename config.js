93a0070cd242f9e5f1c9af6c0dd08d201ee7b0e8
choogoo <104893934+choogoo@users.noreply.github.com>
2022-12-03 17:56:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /* eslint-disable sort-keys */
 // 配置文件，所有配置必须齐全，补充空白配置项，其他配置项可按需要修改
 const configs = {
-  VIKA_TOKEN: 'uskv0Tuj5MxADtcsI1C0Vkh', // VIKA维格表token
-  VIKA_SPACENAME: 'bot-test', // VIKA维格表空间名称，修改为自己的空间名称
+  VIKA_TOKEN: '换成你自己的维格表token', // VIKA维格表token
+  VIKA_SPACENAME: '换成你自己的空间名称', // VIKA维格表空间名称，修改为自己的空间名称
 }
 
 export default configs
