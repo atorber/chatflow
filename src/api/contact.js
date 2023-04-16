ed81a56f76d7405b8c3572ae4606ef448067586f
LuChao <atorber@163.com>
2023-04-16 14:42:47 +0800

    重构目录结构

---
+++ src/api/contact.js
@@ -0,0 +1,24 @@
+import nedb from '../db/nedb.js'
+const db = nedb()
+async function addUser(info) {
+  try {
+    let doc = await db.insert(info)
+    return doc
+  } catch (error) {
+    console.log('插入数据错误', error)
+  }
+}
+async function getUser() {
+  try {
+    let search = await db.find({})
+    return search[0]
+  } catch (error) {
+    console.log('查询数据错误', error)
+  }
+}
+export { addUser }
+export { getUser }
+export default {
+  addUser,
+  getUser,
+}
---
+++ src/lib/tool.ts
@@ -0,0 +1,37 @@
+/* eslint-disable sort-keys */
+import moment from 'moment'
+
+import type {
+  Contact,
+  Room,
+  // ScanStatus,
+  // WechatyBuilder,
+} from 'wechaty'
+
+async function storeSentMessage (userSelf: Contact, text: string, talker?: Contact, room?: Room) {
+  // console.debug('发送的消息：', text)
+  const curTime = new Date().getTime()
+  const timeHms = moment(curTime).format('YYYY-MM-DD HH:mm:ss')
+  const record = {
+    fields: {
+      timeHms,
+      name: userSelf.name(),
+      topic: room ? (await room.topic() || '--') : (talker?.name() || '--'),
+      messagePayload: text,
+      wxid: room && talker ? (talker.id !== 'null' ? talker.id : '--') : userSelf.id,
+      roomid: room ? (room.id || '--') : (talker?.id || '--'),
+      messageType: 'selfSent',
+    },
+  }
+  return record
+}
+
+// 定义一个延时方法
+const waitForMs = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+export {
+  waitForMs,
+  storeSentMessage,
+}
+
+export default waitForMs
