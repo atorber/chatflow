7ae2d5bbfea5a1a83845fd1def7189a5a036fe3e
atorber <atorber@163.com>
2023-11-02 21:10:40 +0800

    Update package-json.ts

--- src/package-json.ts
+++ src/package-json.ts
@@ -1,11 +1,5 @@
 /**
  * This file will be overwrite when we publish NPM module
- * by scripts/generate_version.ts
+ * by scripts/generate_version.js
  */
-import type { PackageJson } from 'type-fest'
-
-/**
- * Huan(202108):
- *  The below default values is only for unit testing
- */
-export const packageJson: PackageJson = {}
+export const packageJson = {}
